fc9c3500ea58b2b0805544c3ef50079f235aa4dc
Clint Moyer <contact@clintmoyer.com>
2026-01-19 18:29:25 -0800

    fix: explicitly load app icon for Command+Tab and Dock

--- Sources/MinesweeperApp.swift
+++ Sources/MinesweeperApp.swift
@@ -5,6 +5,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ notification: Notification) {
         NSApplication.shared.setActivationPolicy(.regular)
         NSApplication.shared.activate(ignoringOtherApps: true)
+
+        // Load icon from bundle for Command+Tab and Dock
+        if let iconURL = Bundle.main.url(forResource: "AppIcon", withExtension: "icns"),
+           let icon = NSImage(contentsOf: iconURL) {
+            NSApplication.shared.applicationIconImage = icon
+        }
     }
 }
 
